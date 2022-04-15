efb5330ead7864d58911eb7fdc58a36604c0409d
Kunal-vajpai <96376111+Kunal-vajpai@users.noreply.github.com>
2022-04-15 16:45:02 +0530

    Add files via upload

---
+++ polling/static/admin.js
@@ -0,0 +1,46 @@
+var dataPoints = [
+      { label: "After 5PM", y: 0 },
+      { label: "Before 5PM", y: 0 },
+    ]
+    
+    var chartContainer = document.querySelector('#chartContainer');
+    
+    if (chartContainer) {
+      var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "theme2",
+        data: [
+          {
+            type: "column",
+            dataPoints: dataPoints
+          }
+        ]
+      });
+      
+      chart.render();
+    }
+    
+    Pusher.logToConsole = true;
+    
+    // Configure Pusher instance
+    const pusher = new Pusher('PUSHER_APP_KEY', {
+      cluster: 'PUSHER_APP_CLUSTER',
+      encrypted: true
+    });
+    
+    // Subscribe to poll trigger
+    var channel = pusher.subscribe('poll');
+    
+    // Listen to vote event
+    channel.bind('vote', function(data) {
+      dataPoints = dataPoints.map(dataPoint => {
+        if(dataPoint.label == data[4].name[0]) {
+          dataPoint.y += 10;
+        }
+    
+        return dataPoint
+      });
+    
+      // Re-render chart
+      chart.render()
+    });
